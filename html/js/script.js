bd9578d4e1ad08b9f0069e8179199989673e655e
andy00784 <64341863+andy00784@users.noreply.github.com>
2021-02-02 05:17:02 +0800

    Update script.js
    JS scroll value changed

--- html/js/script.js
+++ html/js/script.js
@@ -62,7 +62,7 @@ $(function(){
 		$rule_detail_btn.removeClass("selected");
 		$(this).addClass("selected");
 		$("html,body").stop().animate({
-			scrollTop: $("#" + $rule_detail_btn_place).offset().top - 96
+			scrollTop: $("#" + $rule_detail_btn_place).offset().top - 92
 		},800);
 		return false;
 	});
